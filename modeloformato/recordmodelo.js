97150af07cec34b74e48dac0ef15d8528ca62eb8
Jaidan Saul Ibarra <saul.ibarra@espoch.edu.ec>
2026-03-23 09:40:42 -0500

    record academico nueva version

--- modeloformato/recordmodelo.js
+++ modeloformato/recordmodelo.js
@@ -41,3 +41,21 @@ module.exports.ListadoSolicitudesRecordCedula= async function (carrera,cedula) {
     return sendResponseModelo(false, [], error.message)
   }
 }
+
+module.exports.EliminarSolicitudRecord= async function (carrera,idsolicitud,strperiodo,strcedula) {
+  var sentencia = "";
+  sentencia = "delete from [" + carrera + "].[dbo].[tblRecordFirmaAutoridad] where [intRecod] =" + idsolicitud + ""
+  sentencia2 = "delete from [" + carrera + "].[dbo].[tblRecordSolicitud] where [intIdSolicitud]=" + idsolicitud + " and [strCedulaEstudiante]='" + strcedula + "' and [strCodPeriodo]='" + strperiodo + "'"
+  try {
+    if (sentencia != "") {
+      const sqlConsulta = await execDinamico(carrera, sentencia, "OK", "OK");
+      const sqlConsulta2 = await execDinamico(carrera, sentencia2, "OK", "OK");
+        return sendResponseModelo(true, sqlConsulta2, 'OK')
+    } else {
+       return sendResponseModelo(false, [], 'VACIO SQL')
+    }
+  } catch (error) {
+    logger.error('Error EliminarSolicitudRecord', { message: error.message, stack: error.stack });
+    return sendResponseModelo(false, [], error.message)
+  }
+}
--- rutasformato/rutarecord.js
+++ rutasformato/rutarecord.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Request = require("request");
 const procesosfuncionesrecord = require('../procesosformato/procesosrecord');
 const { sendResponseServicios } = require('../herramientas/responseservice'); 
+const logger = require('./../herramientas/logger');
 
 router.get('/ListadoSolicitudesRecordPeriodo/:carrera/:periodo/:estado',async (req, res) => {
     const periodo = req.params.periodo;
@@ -17,7 +18,7 @@ router.get('/ListadoSolicitudesRecordPeriodo/:carrera/:periodo/:estado',async (r
         else {
             return sendResponseServicios(res, true, [],Informacion.message);
         }
-    }catch (err) {
+    }catch (error) {
         logger.error('Error ListadoSolicitudesRecord', { message: error.message, stack: error.stack});
         return sendResponseServicios(res, false, [],err.message);
     }
@@ -35,11 +36,29 @@ router.get('/ListadoSolicitudesRecordCedula/:carrera/:cedula',async (req, res) =
         else {
             return sendResponseServicios(res, true, [],Informacion.message);
         }
-    }catch (err) {
+    }catch (error) {
         logger.error('Error ListadoSolicitudesRecord', { message: error.message, stack: error.stack});
         return sendResponseServicios(res, false, [],err.message);
     }
  
 });
-
+router.get('/EliminarSolicitudRecord/:carrera/:periodo/:cedula/:idsolicitud',async (req, res) => {
+    const cedula = req.params.cedula;
+    const carrera = req.params.carrera;
+    const periodo = req.params.periodo;
+    const idsolicitud = req.params.idsolicitud;
+    try {
+        var Informacion=await  procesosfuncionesrecord.ProcesoEliminarSolicitudRecord(carrera,idsolicitud,periodo,cedula);
+        if (Informacion.proceso) {
+            return sendResponseServicios(res, true, Informacion.datos,'OK');
+        }
+        else {
+            return sendResponseServicios(res, true, [],Informacion.message);
+        }
+    }catch (error) {
+        logger.error('Error EliminarSolicitudRecord', { message: error.message, stack: error.stack});
+        return sendResponseServicios(res, false, [],err.message);
+    }
+ 
+});
 module.exports = router;
